ecc8a3009291f5b025d6724e15c29c4858be429a
Thanakorn Limpanawuthi <thanakorn.limpanawuthi@agoda.com>
2022-10-27 23:59:45 +0700

    Add default color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
         './components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'royal-purple': '#5949d9',
+            },
+        },
     },
     plugins: [],
 }
