284135f0dd6746f0cce4d51fa882b7c2808a8c07
Thanakorn Limpanawuthi <thnk2809@gmail.com>
2022-10-27 17:25:26 +0700

    Navbar change when login

--- components/navbar.js
+++ components/navbar.js
@@ -1,22 +1,59 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {
+    auth,
+    logInWithEmailAndPassword,
+    signInWithGoogle,
+    logout,
+} from '../firebase/config'
 
 export default function Navbar({ fixed }) {
     const [navbarOpen, setNavbarOpen] = React.useState(false)
+    const [currentUser, setCurrentUser] = useState(null)
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                setCurrentUser(user)
+            }
+        })
+    })
     return (
         <div className='bg-gradient-to-r from-cyan-500 to-blue-500 text-slate-50'>
             <nav className='flex justify-between px-[10rem] py-[1.5rem]'>
                 <div className='text-3xl font-bold'>Mutelu</div>
                 <ul className='menu flex '>
-                    <li className='text-xl mx-[40px] pt-1'>
-                        <Link href='/'>Home</Link>
-                    </li>
-                    <li className='text-xl mx-[40px] pt-1'>
-                        <Link href='/login'>Log In</Link>
-                    </li>
-                    <li className='text-xl mx-[40px] pt-1'>
-                        <Link href='/signup'>Sign Up</Link>
-                    </li>
+                    {currentUser ? (
+                        <>
+                            <li className='text-xl mx-[40px] pt-1'>
+                                <Link href='/'>Home</Link>
+                            </li>
+                            <li className='text-xl mx-[40px] pt-1'>
+                                <Link href='/Search Sanctuary'>
+                                    Search Sanctuary
+                                </Link>
+                            </li>
+                            <li className='text-xl mx-[40px] pt-1'>
+                                <button onClick={logout}>Log Out</button>
+                            </li>{' '}
+                        </>
+                    ) : (
+                        <>
+                            <li className='text-xl mx-[40px] pt-1'>
+                                <Link href='/'>Home</Link>
+                            </li>
+                            <li className='text-xl mx-[40px] pt-1'>
+                                <Link href='/Search Sanctuary'>
+                                    Search Sanctuary
+                                </Link>
+                            </li>
+                            <li className='text-xl mx-[40px] pt-1'>
+                                <Link href='/login'>Log In</Link>
+                            </li>
+                            <li className='text-xl mx-[40px] pt-1'>
+                                <Link href='/signup'>Sign Up</Link>
+                            </li>
+                        </>
+                    )}
                 </ul>
             </nav>
         </div>
