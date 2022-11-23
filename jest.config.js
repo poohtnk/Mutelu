6411201afaa916d93d0e6585d62bfd8d44b52ef0
Thanakorn Limpanawuthi <thnk2809@gmail.com>
2022-11-24 01:30:54 +0700

    try add badges

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
     moduleDirectories: ['node_modules', '<rootDir>/'],
     testEnvironment: 'jest-environment-jsdom',
+    coverageReporters: ['json-summary', 'text', 'lcov'],
 }
 
 module.exports = createJestConfig(customJestConfig)
